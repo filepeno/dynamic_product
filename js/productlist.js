a1fcebb66f165c11efdfbbd7be75af667cf2dfa1
Filip Soudakov <fili4252@stud.kea.dk>
2021-02-08 13:38:34 +0100

    made template in productlist

--- js/productlist.js
+++ js/productlist.js
@@ -13,8 +13,35 @@ function handleProductList(data) {
   data.forEach(showProduct);
 }
 
+/* <template id="templateProduct">
+<h1 class="heading">Apparel</h1>
+<section class="productList">
+  <article class="smallProduct">
+    <img
+      src="https://kea-alt-del.dk/t7/images/webp/640/1163.webp"
+      alt="Sahara Team India Fanwear Round Neck Jersey"
+    />
+    <h4 class="brand">Nike</h4>
+    <h3 class="item_title">
+      Sahara Team India Fanwear Round Neck Jersey
+    </h3>
+    <div class="price_area">
+      <div class="price">
+        <span>Prev.</span>
+        <h3>599</h3>
+        DKK
+      </div>
+      <div class="discounted">
+        Now<span> <h3>300</h3></span> DKK
+      </div>
+    </div>
+    <a href="product.html">Read more</a>
+  </article>
+</template> */
+
 function showProduct(item) {
   //grab template
+  const template = document.querySelector("#templateProduct");
   //clone it
   //change content
   //grab parent
