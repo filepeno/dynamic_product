3b7085323e29d8e5b9c1b9c2ebd35d6bd9f883e3
Filip Soudakov <fili4252@stud.kea.dk>
2021-02-08 13:33:57 +0100

    changed productlist to section, commented out all but 1st product, added JS to productlist

---
+++ js/productlist.js
@@ -0,0 +1,22 @@
+const url = "https://kea-alt-del.dk/t7/api/products";
+
+fetch(url)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    handleProductList(data);
+  });
+
+function handleProductList(data) {
+  console.log(data);
+  data.forEach(showProduct);
+}
+
+function showProduct(item) {
+  //grab template
+  //clone it
+  //change content
+  //grab parent
+  //append
+}
